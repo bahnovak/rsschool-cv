c7672af735ebe8f431c3b8e41d96f462c6d37d86
bahnovak <46083110+bahnovak@users.noreply.github.com>
2020-02-10 11:53:58 +0300

    feat: add js

---
+++ js.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',function(){
+    let body = document.querySelector('body'),
+        img = document.querySelector('.img'),
+        cv = document.querySelector('.cv');
+    
+    body.classList.add('opa');
+    img.classList.add('img_opa');
+    cv.classList.add('cv_opa');
+
+});
