de2c8f64226e732f69423b540bfc946815c7d742
bahnovak <46083110+bahnovak@users.noreply.github.com>
2020-02-11 01:32:21 +0300

    fix: change js

--- js.js
+++ js.js
@@ -7,9 +7,7 @@ window.addEventListener('load',function(){
     body.classList.add('opa');
     img.classList.add('img_opa');
     cv.classList.add('cv_opa');
-    setTimeout(() => {
-        con.classList.add('contain_opa');
-    }, 1000); 
+    con.classList.add('contain_opa');
 
 });
 
